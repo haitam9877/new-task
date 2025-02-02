a494b772f2ee6731c1b3759eca7b73bd78cd156c
boubaouheithem <azehaitam@gmail.com>
2025-02-02 08:09:53 +0100

    f

--- admin/users.php
+++ admin/users.php
@@ -19,14 +19,19 @@ $usersCont = $sit->rowCount();
 
 
 
-<div class="container-teble  p-4" id="pry-color">
+<div class="container-teble  p-4" >
 
-<h2 class="text-center">Users Mangament</h2>
 
-    <div class="container">
 
-    <span class="mt-4 mb-4">users   </span>  <strong class="btn btn-primary"><?php echo $usersCont ?></strong>
-    <table class="table table-light">
+    <div class="container mt-5">
+
+    <h3 class="h3">
+  Users <span class="badge bg-secondary"><?php echo $usersCont; ?></span>
+</h3>
+
+
+  
+    <table class="table table-striped mt-2 table-light table-bordered">
   <thead>
  
    
@@ -42,20 +47,80 @@ $usersCont = $sit->rowCount();
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
-      <td>@mdo</td>
+    
+  <?php
+
+if($usersCont > 0){
+
+
+  foreach($users as $user){
+
+?>
+  <tr>
+      <th scope="row"><?php echo $user['id'] ?></th>
+      <td><?php echo $user['username'] ?></td>
+      <td><?php echo $user['email'] ?></td>
+      <td><p class="badge bg-primary"><?php echo $user['role'] ?></p></td>
+      <td><?php 
+      
+      if($user['status'] === "1"){
+
+       echo '<p class="badge bg-success">Active</p>';
+      }else{
+        echo '<p class="badge bg-danger">Pindding</p>';
+      }
+      
+      
+      ?></td>
+      <td>
+
+      <div class="contr">
+       
+        <a type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        <i class="fa-solid fa-circle-info"></i>
+        </a>
+
+        <a href="#" class="btn btn-danger">
+        <i class="fa-solid fa-trash"></i>
+        </a>
+      </div>
+
+      </td>
     </tr>
+
+    <?php
+  }
+ }
+?>
     
   </tbody>
 </table>
+
+
     </div>
 </div>
 
+
+
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
 <?php
