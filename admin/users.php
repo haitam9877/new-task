2a29d9df207594088b07890e1d46f5462e0eb30c
heithem <azehaitam@gmail.com>
2025-02-04 22:15:08 +0100

    1

--- admin/users.php
+++ admin/users.php
@@ -7,25 +7,51 @@ include 'includes/template/navbar.php';
 
 
 
+// check Page
 
+if(isset($_GET['page'])){
+  $page = $_GET['page'];
+}else{
+  $page = "All";
+
+}
+
+
+
+// Get users Data
 
 $sit = $conn->prepare("SELECT * FROM users");
 $sit->execute();
 $users = $sit->fetchAll();
 $usersCont = $sit->rowCount();
 
+
+
+
+
 ?>
 
 
-
+<?php if($page == "All"){ 
+  
+  
+  
+  ?>
 
 <div class="container-teble  p-4">
 
+<h1 class="text-center">Users Mangemante</h1>
 
 
-  <div class="container mt-5">
 
-    <h3 class="h3">
+
+  <div class="container mt-2">
+
+  <div class="add-user mt-4 mb-4 ">
+    <a href="?page=addUser" class="btn btn-primary">Add User</a>
+  </div>
+
+    <h3 >
       Users <span class="badge bg-secondary"><?php echo $usersCont; ?></span>
     </h3>
 
@@ -91,19 +117,14 @@ $usersCont = $sit->rowCount();
             </tr>
 
         <?php
+            }
           }
-        }
+        
         ?>
-
       </tbody>
     </table>
-
-
   </div>
 </div>
-
-
-
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -124,8 +145,129 @@ $usersCont = $sit->rowCount();
   </div>
 </div>
 
+<?php }elseif($page == "addUser"){ 
+  
+ 
+  
+  
+  ?>
 
+  <div class="container">
+    <h3  class=" mt-5 mb-5">Regster</h3>
+    <div class=" w-50 ">
+            <form method="post" action="?page=seveUser">
+                <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Username</label>
+                    <input type="text" class="form-control" name="username">
+               
+                 </div>
+
+                 <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="text" class="form-control" name="email">
+               
+                 </div>
+                <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" name="password">
+                </div>
+
+                <div class="mb-3">
+                <label for="exampleInputPassword1" class="form-label">Role</label>
+
+                <select class="form-select" aria-label="Default select example" class="w-20" name="role">
+                  <option selected>Choose</option>
+                  <option value="admin">Admin</option>
+                  <option value="user">User</option>
+                 
+                </select>
+                </div>
+               
+            <button type="submit" class="btn btn-primary" name="add-user">Seve</button>
+        </form>
+    </div>
+</div>
+
+
+
+
+  <?php }elseif($page == "seveUser") {
+
+
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+      if(isset($_POST["add-user"])){
+
+        $usernameErr= $emailErr=$passwordErr=$roleErr = "";
+
+        $username = $_POST["username"];
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+        $role = $_POST["role"];
+
+
+        if(!empty($username)){
+          $username = htmlspecialchars($username, ENT_QUOTES,"UTF-8");
+        }else{
+          $usernameErr = "the Username Is Empty";
+        }
+
+        if(!empty($email)){
+          $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+        }else{
+          $emailErr = "the Email Is Empty";
+        }
+
+        if(!empty($password)){
+          $password = htmlspecialchars($password, ENT_QUOTES,"UTF-8");
+        }else{
+          $passwordErr = "the Password Is Empty";
+        }
+
+        if(!empty($role)){
+          $role = htmlspecialchars($role, ENT_QUOTES,"UTF-8");
+        }else{
+          $roleErr = "the Role Is Empty";
+        }
+
+
+        if(empty($usernameErr) && empty($emailErr)  && empty($passwordErr) && empty($roleErr)){
+
+          $sita = $conn->prepare("INSERT INTO users (username, email, password, status, role, created_at) 
+        VALUES (:zusername, :zemail, :zpassword, :zstatus, :zrole, NOW())");
+          $sita->execute([
+            ':zusername' => $username,
+            ':zemail'    => $email,
+            ':zpassword' => $password,
+            ':zstatus'   => '0',
+            ':zrole'     => 'admin'
+        ]);
+
+          if($sita->rowCount() > 0){
+
+
+            echo "you have ben sassisfoly";
+            header('Location:users.php');
+            exit();
+
+          } 
+
+
+        }else{
+          echo  $emailErr;
+        }
+  
+    }
+  }
+
+  ?>
+
+
+
+
+  
 <?php
-
+  }
 include 'includes/template/footer.php';
 ?>
